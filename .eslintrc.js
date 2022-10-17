d1f8a9c28c3b4a8fbe39d910c5127197701fc490
trikunc <kurookun@gmail.com>
2022-10-17 09:15:55 +0700

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    // 'eslint:recommended',
+    // 'plugin:react/recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'prettier',
+  ],
+  // rules: {
+  //   'no-unused-vars': 'off',
+  //   '@typescript-eslint/no-unused-vars': ['error'],
+  //   '@typescript-eslint/no-var-requires': 'off',
+  //   'react/prop-types': 'off',
+  //   'react/jsx-uses-react': 'off',
+  //   'react/react-in-jsx-scope': 'off',
+  //   '@typescript-eslint/explicit-module-boundary-types': 'off',
+  // },
 };
--- App.js
+++ App.js
@@ -1,94 +1,191 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+// import type { Node } from 'react';
 import {
+  Image,
+  Platform,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import axios from 'axios';
+// const axios = require('axios');
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const createFormData = (imageCamera, body = {}) => {
+  const data = new FormData();
+
+  data.append('file', {
+    name: imageCamera.fileName,
+    type: imageCamera.type,
+    uri: Platform.OS === 'ios' ? imageCamera.uri.replace('file://', '') : imageCamera.uri,
+  });
+
+  Object.keys(body).forEach((key) => {
+    data.append(key, body[key]);
+  });
+
+  return data;
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [imageCamera, setImageCamera] = useState(null);
+  const [Latitude, setLatitude] = useState();
+  const [Longitude, setLongitude] = useState();
+  const [dataFetch, setDataFetch] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // const urlFetch = 'http://192.168.8.166:8080';
+  const urlFetch = 'http://10.6.172.16:8080';
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`${urlFetch}/api/getSingleFile/1`);
+      const json = await response.json();
+      console.log('dataFetch =', json);
+      setDataFetch(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+
+      setLatitude(lat);
+      setLongitude(long);
+
+      getData();
+
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading]);
+
+  const openCamera = () => {
+    const options = {
+      mediaType: 'photo',
+      quality: 1,
+    };
+
+    launchCamera(options, (res) => {
+      if (res.didCancel) {
+        console.log('User canceled image');
+      } else if (res.errorCode) {
+        console.log(res.errorMessage);
+      } else {
+        const data = res.assets[0];
+        setImageCamera(data);
+        console.log(data);
+      }
+    });
+  };
+
+  const openGallery = () => {
+    const options = {
+      mediaType: 'photo',
+      quality: 1,
+    };
+
+    launchImageLibrary(options, (res) => {
+      if (res.didCancel) {
+        console.log('User canceled image');
+      } else if (res.errorCode) {
+        console.log(res.errorMessage);
+      } else {
+        const data = res.assets[0];
+        setImageCamera(data);
+      }
+    });
+  };
+
+
+  const saveImage = async (e) => {
+
+    setLoading(true);
+    const imgUrl = await fetch(`${urlFetch}/api/upload`, {
+      method: 'POST',
+      body: createFormData(imageCamera, { userId: '1' }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log('response', response);
+        return response;
+      })
+      .catch((error) => {
+        console.log('error', error.message);
+        throw error;
+      });
+
+    try {
+      await axios.put(`${urlFetch}/api/singleFile/${dataFetch.id}`, {
+        email: dataFetch.email,
+        name: dataFetch.name,
+        filePath: imageCamera ? imgUrl : '',
+        longitude: `${Longitude}`,
+        latitude: `${Latitude}`,
+      });
+
+      getData();
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+    <SafeAreaView>
+      <ScrollView>
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          style={styles.container}
+        >
+          {dataFetch != null &&
+            <View style={{ ...styles.card, backgroundColor: 'skyblue' }}>
+              <Text>Image from Back End</Text>
+              <Image source={{ uri: `${urlFetch}/uploads/${dataFetch.filePath}` }} style={styles.image} />
+              <View>
+                <Text>Latitude: {dataFetch.latitude}</Text>
+                <Text>Longitde: {dataFetch.longitude}</Text>
+              </View>
+            </View>
+
+          }
+          <View style={{ ...styles.card }}>
+            <Text>Take Image</Text>
+
+            {imageCamera != null &&
+              <Image source={{ uri: imageCamera.uri }} style={styles.image} />
+
+            }
+
+            {Latitude != null &&
+              <View>
+                <Text>Latitude: {Latitude}</Text>
+                <Text>Longitde: {Longitude}</Text>
+
+              </View>
+            }
+            <TouchableOpacity style={styles.button} onPress={openCamera}>
+              <Text style={styles.textWhite}>Open Camera</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={openGallery}>
+              <Text style={styles.textWhite}>Open Gallery</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <TouchableOpacity style={{ ...styles.button, borderRadius: 12 }} onPress={saveImage}>
+            <Text style={{ ...styles.textWhite, fontSize: 24 }}>SAVE</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -96,22 +193,34 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  card: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: 'green',
+    color: 'white',
   },
-  highlight: {
-    fontWeight: '700',
+  textWhite: {
+    color: 'white',
   },
+  image: {
+    height: 100,
+    width: 100,
+  },
+
+
+
 });
 
 export default App;
